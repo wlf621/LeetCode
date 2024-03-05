e559447363a812266ae908e9a0a05b2e67a328f0
wlf <cxwanglunfei@163.com>
2024-03-05 22:21:43 +0800

    100

--- inc.h
+++ inc.h
@@ -9,6 +9,17 @@
 #include <unordered_set>
 #include <unordered_map>
 
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 using namespace std;
 
 struct ListNode {
